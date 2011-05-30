f83eb7ff8050353962fa7f9079cfcc07739d14c9
Aleksey Yatsiuk <e-mail@feedb.org>
2011-05-30 23:00:08 +0400

    add icons to panel

--- resource_monitor@e-mail.feedb.org/extension.js
+++ resource_monitor@e-mail.feedb.org/extension.js
@@ -2,6 +2,7 @@
 
 // system-monitor: Gnome shell extension displaying system informations in gnome shell status bar, such as memory usage, cpu usage, network rates…
 // Copyright (C) 2011 Florian Mounier aka paradoxxxzero
+// Copyright (C) 2011 Aleksey Yatsiuk e-mail@feedb.org
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,6 +18,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Author: Florian Mounier aka paradoxxxzero
+//          Aleksey Yatsiuk e-mail@feedb.org
 
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
@@ -32,6 +34,19 @@ function SystemMonitor() {
     this._init.apply(this, arguments);
 }
 
+function bytes_to_string (speed) {
+  
+  var pristavka = "";
+
+  if (speed < 1024) {
+  	pristavka = "KiB/s"
+  } else if (speed >= 1024) {
+  	speed = speed / 1024;
+  	pristavka = "MiB/s"
+  }
+  return (speed.toFixed(1) + pristavka);
+}
+
 SystemMonitor.prototype = {
     __proto__: PanelMenu.SystemStatusButton.prototype,
     icon_size: Math.round(Panel.PANEL_ICON_SIZE * 4 / 5),
@@ -39,6 +54,15 @@ SystemMonitor.prototype = {
     _init_menu: function() {
         let section = new PopupMenu.PopupMenuSection("Usages");
         this.menu.addMenuItem(section);
+
+        item = new PopupMenu.PopupMenuItem("CPU");
+        item.addActor(new St.Label({ text:':', style_class: "sm-label"}));
+        this._cpu = new St.Label({ style_class: "sm-value"});
+        item.addActor(new St.Label({ style_class: "sm-void"}));
+        item.addActor(new St.Label({ style_class: "sm-void"}));
+        item.addActor(this._cpu);
+        item.addActor(new St.Label({ text:'%', style_class: "sm-label"}));
+        section.addMenuItem(item);
 
         let item = new PopupMenu.PopupMenuItem("Memory");
         this._mem = new St.Label({ style_class: "sm-value"});
@@ -48,6 +72,7 @@ SystemMonitor.prototype = {
         item.addActor(new St.Label({ text: "/", style_class: "sm-label"}));
         item.addActor(this._mem_total);
         item.addActor(new St.Label({ text: "M", style_class: "sm-label"}));
+        
         section.addMenuItem(item);
 
         item = new PopupMenu.PopupMenuItem("Swap");
@@ -58,15 +83,6 @@ SystemMonitor.prototype = {
         item.addActor(new St.Label({ text: "/", style_class: "sm-label"}));
         item.addActor(this._swap_total);
         item.addActor(new St.Label({ text: "M", style_class: "sm-label"}));
-        section.addMenuItem(item);
-
-        item = new PopupMenu.PopupMenuItem("Cpu");
-        item.addActor(new St.Label({ text:':', style_class: "sm-label"}));
-        this._cpu = new St.Label({ style_class: "sm-value"});
-        item.addActor(new St.Label({ style_class: "sm-void"}));
-        item.addActor(new St.Label({ style_class: "sm-void"}));
-        item.addActor(this._cpu);
-        item.addActor(new St.Label({ text:'%', style_class: "sm-label"}));
         section.addMenuItem(item);
 
         item = new PopupMenu.PopupMenuItem("Net");
@@ -83,6 +99,14 @@ SystemMonitor.prototype = {
 
         section = new PopupMenu.PopupMenuSection("Toggling");
         this.menu.addMenuItem(section);
+	    this._cpu_widget = new PopupMenu.PopupSwitchMenuItem("Display cpu", true);
+	    this._cpu_widget.connect('toggled', Lang.bind(this, function(item) {
+	                                                      this._cpu_box.visible = item.state;
+	                                                      if(this._schema) {
+		                                                      this._schema.set_boolean("cpu-display", item.state);
+	                                                      }
+                                                      }));
+        section.addMenuItem(this._cpu_widget);
 	    this._mem_widget = new PopupMenu.PopupSwitchMenuItem("Display memory", true);
 	    this._mem_widget.connect('toggled', Lang.bind(this, function(item) {
 	                                                      this._mem_box.visible = item.state;
@@ -99,14 +123,6 @@ SystemMonitor.prototype = {
 	                                                       }
                                                        }));
         section.addMenuItem(this._swap_widget);
-	    this._cpu_widget = new PopupMenu.PopupSwitchMenuItem("Display cpu", true);
-	    this._cpu_widget.connect('toggled', Lang.bind(this, function(item) {
-	                                                      this._cpu_box.visible = item.state;
-	                                                      if(this._schema) {
-		                                                      this._schema.set_boolean("cpu-display", item.state);
-	                                                      }
-                                                      }));
-        section.addMenuItem(this._cpu_widget);
 	    this._net_widget = new PopupMenu.PopupSwitchMenuItem("Display net", true);
 	    this._net_widget.connect('toggled', Lang.bind(this, function(item) {
 	                                                      this._net_box.visible = item.state;
@@ -118,41 +134,42 @@ SystemMonitor.prototype = {
     },
     _init_status: function() {
         let box = new St.BoxLayout();
-        let icon = new St.Icon({ icon_type: St.IconType.SYMBOLIC, icon_size: this.icon_size, icon_name:'utilities-system-monitor'});
         this._mem_ = new St.Label({ style_class: "sm-status-value"});
         this._swap_ = new St.Label({ style_class: "sm-status-value"});
         this._cpu_ = new St.Label({ style_class: "sm-status-value"});
         this._netdown_ = new St.Label({ style_class: "sm-big-status-value"});
         this._netup_ = new St.Label({ style_class: "sm-big-status-value"});
 
-        box.add_actor(icon);
+        let cpu_icon = new St.Icon({ icon_type: St.IconType.FULLCOLOR, icon_size: this.icon_size, icon_name:'cpu-icon'});
+        let mem_icon = new St.Icon({ icon_type: St.IconType.FULLCOLOR, icon_size: this.icon_size, icon_name:'mem-icon'});
+        let swap_icon = new St.Icon({ icon_type: St.IconType.FULLCOLOR, icon_size: this.icon_size, icon_name:'swap-icon'});
+        let net_icon = new St.Icon({ icon_type: St.IconType.FULLCOLOR, icon_size: this.icon_size, icon_name:'net-icon'});
+
+
+	    this._cpu_box = new St.BoxLayout();
+        this._cpu_box.add_actor(cpu_icon);
+        this._cpu_box.add_actor(this._cpu_);
+        this._cpu_box.add_actor(new St.Label({ text: '%', style_class: "sm-perc-label"}));
+	    box.add_actor(this._cpu_box);
 
 	    this._mem_box = new St.BoxLayout();
-        this._mem_box.add_actor(new St.Label({ text: 'mem', style_class: "sm-status-label"}));
+        this._mem_box.add_actor(mem_icon);
         this._mem_box.add_actor(this._mem_);
         this._mem_box.add_actor(new St.Label({ text: '%', style_class: "sm-perc-label"}));
 	    box.add_actor(this._mem_box);
 
 	    this._swap_box = new St.BoxLayout();
-        this._swap_box.add_actor(new St.Label({ text: 'swap', style_class: "sm-status-label"}));
+        this._swap_box.add_actor(swap_icon);
         this._swap_box.add_actor(this._swap_);
         this._swap_box.add_actor(new St.Label({ text: '%', style_class: "sm-perc-label"}));
 	    box.add_actor(this._swap_box);
 
-	    this._cpu_box = new St.BoxLayout();
-        this._cpu_box.add_actor(new St.Label({ text: 'cpu', style_class: "sm-status-label"}));
-        this._cpu_box.add_actor(this._cpu_);
-        this._cpu_box.add_actor(new St.Label({ text: '%', style_class: "sm-perc-label"}));
-	    box.add_actor(this._cpu_box);
-
 	    this._net_box = new St.BoxLayout();
-        this._net_box.add_actor(new St.Label({ text: 'net', style_class: "sm-status-label"}));
+        this._net_box.add_actor(net_icon);
         this._net_box.add_actor(new St.Icon({ icon_type: St.IconType.SYMBOLIC, icon_size: 2 * this.icon_size / 3, icon_name:'go-down'}));
         this._net_box.add_actor(this._netdown_);
-        this._net_box.add_actor(new St.Label({ text: 'kB/s', style_class: "sm-unit-label"}));
         this._net_box.add_actor(new St.Icon({ icon_type: St.IconType.SYMBOLIC, icon_size: 2 * this.icon_size / 3, icon_name:'go-up'}));
         this._net_box.add_actor(this._netup_);
-        this._net_box.add_actor(new St.Label({ text: 'kB/s', style_class: "sm-unit-label"}));
 	    box.add_actor(this._net_box);
 
         this.actor.set_child(box);
@@ -324,12 +341,14 @@ SystemMonitor.prototype = {
 	        let time = GLib.get_monotonic_time() / 1000;
 	        if(this.__last_net_time != 0) {
 		        let delta = time - this.__last_net_time;
-		        let net_down = Math.round((down - this.__last_net_down) / delta);
-		        let net_up = Math.round((up - this.__last_net_up) / delta);
-		        this._netdown_.set_text(net_down.toString());
-		        this._netup_.set_text(net_up.toString());
-		        this._netdown.set_text(net_down + " kB/s");
-		        this._netup.set_text(net_up + " kB/s");
+		        let net_down = (down - this.__last_net_down) / delta;
+		        let net_up = (up - this.__last_net_up) / delta;
+
+        
+		        this._netdown_.set_text(bytes_to_string(net_down));
+		        this._netup_.set_text(bytes_to_string(net_up));
+		        this._netdown.set_text(bytes_to_string(net_down));
+		        this._netup.set_text(bytes_to_string(net_up));
 	        }
 	        this.__last_net_down = down;
 	        this.__last_net_up = up;
